1bed4d0f2243d8d6d6ee2f7cf26db55c380aa826
Nikolay Shelevoy <runtycybin@gmail.com>
2026-09-09 11:29:54 +0300

    AuthController refactor

--- iwanit/src/main/java/com/nicodev/iwanit/controller/AuthController.java
+++ iwanit/src/main/java/com/nicodev/iwanit/controller/AuthController.java
@@ -1,21 +1,14 @@
 package com.nicodev.iwanit.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nicodev.iwanit.model.User;
 import com.nicodev.iwanit.model.dto.AuthRequestDto;
 import com.nicodev.iwanit.model.dto.AuthResponseDto;
 import com.nicodev.iwanit.model.dto.RegisterRequestDto;
-import com.nicodev.iwanit.repository.UserRepository;
-import com.nicodev.iwanit.security.JwtService;
 import com.nicodev.iwanit.service.UserService;
 
 import lombok.AllArgsConstructor;
