0766db8d89936bd457df3b75bc3b559f3ea69ea7
Nikolay Shelevoy <runtycybin@gmail.com>
2026-09-22 13:15:07 +0300

    new authorized path for errors

--- iwanit/src/main/java/com/nicodev/iwanit/configuration/SecurityConfig.java
+++ iwanit/src/main/java/com/nicodev/iwanit/configuration/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
         .csrf(csrf -> csrf.disable())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/v1/auth/**").permitAll()
+            .requestMatchers("/v1/auth/**", "/error").permitAll()
             .anyRequest().authenticated())
         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
