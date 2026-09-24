8c07e150aaec634e9cb829d42ff9815c6e951cac
Nikolay Shelevoy <runtycybin@gmail.com>
2026-09-24 19:57:45 +0300

    main view created

---
+++ iwanit_front/src/router/index.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../views/Login.vue'
+import Home from '../views/Home.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/login', name: 'login', component: Login },
+    { path: '/', name: 'home', component: Home },
+  ],
+})
+
+export default router
